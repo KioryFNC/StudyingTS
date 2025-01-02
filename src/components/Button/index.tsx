fdfadde37f3bb3f15bc1071071357276cba6cddb
yuri <yuri.barros.luz.familia@gmail.com>
2025-01-02 17:19:59 -0300

    button open and close div

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import * as Element from './styles'
 
+
 export function Button() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsOpen(true)
+  }
+
+  const handleClose = () => {
+    setIsOpen(false)
+  }
   return (
-    <Element.Button>
-      open
-    </Element.Button>
+    <>
+      <Element.Button onClick={handleButtonClick}>
+        open
+      </Element.Button>
+
+      {isOpen && (
+        <Element.ModalOverlay>
+          <Element.ModalContent>
+            <h2>Conteúdo da Div</h2>
+            <p>Texto ou outros componentes dentro da div</p>
+            <button onClick={handleClose}>Fechar</button>
+          </Element.ModalContent>
+        </Element.ModalOverlay>
+      )}
+    </>
   )
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,19 +1,18 @@
 import styled from "styled-components" 
 
 export const Button = styled.button`
- background: linear-gradient(90deg, rgb(240, 67, 139) 0%, #8b5cf6 100%);
-  background-clip: text;
-  -webkit-background-clip: text; 
-  -webkit-text-fill-color: transparent;
-  
   font-size: 3rem;
   font-weight: bold;
   border: 2px solid transparent;
   border-radius: 8px;
   padding: 1.2rem 2.4rem;
   cursor: pointer;
-  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
   text-transform: uppercase;
+  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+  background: linear-gradient(90deg, rgb(240, 67, 139) 0%, #8b5cf6 100%);
+  background-clip: text;
+  -webkit-background-clip: text; 
+  -webkit-text-fill-color: transparent;
 
   &:hover {
     transform: scale(1.05); 
@@ -28,5 +27,61 @@ export const Button = styled.button`
     outline: none;
     box-shadow: 0 0 8px rgba(240, 67, 139, 0.6);
   }
+`
 
+export const ModalOverlay = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  backdrop-filter: blur(.5rem);
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+`
+
+export const ModalContent = styled.div`
+  width: 100%;
+  max-width: 50rem;
+  height: 40rem;
+  padding: 2rem;
+  text-align: center;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 6px;
+  background: white;
+  display: flex;
+  flex-direction: column;
+
+  >h2 {
+    font-size: 2rem;
+    margin-bottom: 2rem;
+    color: #8b5cf6;
+  }
+
+  >p {
+    font-size: 1.4rem;
+    margin-bottom: 2rem;
+    color: #8b5cf6;
+  }
+
+  >button {
+    margin:  auto 0 0 auto;
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 2rem;
+    padding: 1rem 2rem;
+    border: none;
+    cursor: pointer;
+    border-radius: 5rem;
+    transition: background-color 0.3s, ease-in-out;
+    background: linear-gradient(90deg,rgb(240, 67, 139) 0%, #8b5cf6 100%);
+    
+    &:hover {
+      transform: scale(1.1);
+      background: linear-gradient(90deg, #8b5cf6 0%,rgb(240, 67, 139) 100%);
+    }
+  }
 `
