611825d818f508c478eda5e13e0dfc8b917a39c3
yuri <yuri.barros.luz.familia@gmail.com>
2025-01-07 13:27:08 -0300

    UserCard.tsx

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,11 +1,22 @@
 import { Games } from "../components/Games"
 import { Grid } from "../components/Grid"
 import { Header } from "../components/Header"
+import { UserCard } from "../components/UserCard"
 import * as Element from "./styles"
 
 export function Home() {
   return (
     <Element.Root>
+      <UserCard 
+        id={55555}
+        nickName="KioryFNC"
+        name="Yuri Barros"
+        profession="Desenvolvedor"
+        age={23}
+        balance={10000}
+        phone="(11)91234-5678"
+        imageUrl="https://github.com/KioryFNC.png"
+      />
       <Header />
       <Games />
       <Grid />
---
+++ src/components/UserCard/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Root = styled.div``
+export const CardHeader = styled.div``
+export const ProfileImage = styled.img``
+export const CardBody = styled.div``
+export const CardItem = styled.p``
